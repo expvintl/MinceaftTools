07a266d9285a4724404ade18fe0e7f9e80d38623
expvintl <expvintl@gmail.com>
2024-08-20 17:04:00 +0800

    添加玩家延迟颜色

--- src/main/java/com/expvintl/mctools/modules/PlayerListTextLatency.java
+++ src/main/java/com/expvintl/mctools/modules/PlayerListTextLatency.java
@@ -11,12 +11,24 @@ public class PlayerListTextLatency {
     public void init(){
         MCEventBus.INSTANCE.register(INSTANCE);
     }
+    private int calcLatencyColor(int latency){
+        if(latency>=0&&latency<=60){ //0-60
+            return 0x00FF00; //绿色
+        }else if(latency>60&&latency<=120){ //60-120
+            return 0xFFFF00; //黄色
+        }else if(latency>120&&latency<=200){//120-200
+            return 0xFFA500; //橙色
+        }else if(latency>200){ //>200
+            return 0xFF0000; //红色
+        }
+        return 0xFFFFFF; //默认白色
+    }
     @Subscribe
     public void onRenderLatencyIcon(RenderLatencyIconEvent event){
         TextRenderer renderer=MinecraftClient.getInstance().textRenderer;
-        int latency=Math.clamp(event.entry.getLatency(),0,9999);
+        int latency=Math.clamp(event.entry.getLatency(),0,999);
         String text=latency+" ms";
-        event.draw.drawTextWithShadow(renderer,text, event.x+event.width-renderer.getWidth(text),event.y,0x00E970);
+        event.draw.drawTextWithShadow(renderer,text, event.x+event.width-renderer.getWidth(text),event.y,calcLatencyColor(latency));
         event.callback.cancel();
     }
 }
